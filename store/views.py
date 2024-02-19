024c0b714f5cebbf8d1842d1d1c1607d1bc45119
Gowtham P <106870998+GowthamOptom@users.noreply.github.com>
2024-02-20 00:15:38 +0530

    part-2 completed

--- store/views.py
+++ store/views.py
@@ -8,9 +8,26 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete = False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_order_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        # Create an empty cart for the non-logged user
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        items = []
+    context = {'items':items, 'order': order}
     return render(request, 'store/checkout.html', context)
